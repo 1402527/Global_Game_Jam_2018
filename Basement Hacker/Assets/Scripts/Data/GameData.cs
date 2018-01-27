d0249819c973d55a5acc8f5becdb1cba0d56523c
Thomas Haygarth <haygarth.thomas@gmail.com>
2018-01-27 02:09:44 +0000

    Added basic data structures for  Tasks and the game data itself.

---
+++ Basement Hacker/Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HacOS.Scripts.Data {
+    [CreateAssetMenuAttribute(fileName ="gameData", menuName ="HacOS/GameData", order = 0)]
+	public class GameData : ScriptableObject {
+		public TaskBank[] taskBanks;
+	}
+}
---
+++ Basement Hacker/Assets/Scripts/Data/TaskBank.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HacOS.Scripts.Data {
+    [CreateAssetMenuAttribute(fileName ="TaskBank", menuName ="HacOS/TaskBank", order = 2)]
+	public class TaskBank : ScriptableObject {
+		public bool isSequential = true;
+        public UserChoice[] Tasks;
+    }
+}
