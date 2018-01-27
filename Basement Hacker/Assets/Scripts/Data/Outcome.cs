a647716c4937b7d4cef269f375eaa4327540a3e0
Thomas Haygarth <haygarth.thomas@gmail.com>
2018-01-27 12:39:31 +0000

    Added in the TexArea attribute to make it possible to use multiline string with the data

--- Basement Hacker/Assets/Scripts/Data/Outcome.cs
+++ Basement Hacker/Assets/Scripts/Data/Outcome.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace HacOS.Scripts.Data {
     [CreateAssetMenuAttribute(fileName ="choice", menuName ="HacOS/Outcome", order = 1)]
     public class Outcome : ScriptableObject {
-        public string text;
+        [TextArea] public string text;
 		[Range(-50,50)]
         public int outcomeValue;
     }
