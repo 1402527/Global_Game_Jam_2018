e7b2e9a6d4c6488303a8750648fa4844edfaed35
Thomas Haygarth <haygarth.thomas@gmail.com>
2018-01-27 14:26:44 +0000

    Added in the beginnins of the UIController and the transmition controller for interacting between the game and ui

---
+++ Basement Hacker/Assets/Scripts/UI/TransmitionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HacOS.Scripts.UI {
+	public class TransmitionController : MonoBehaviour {
+		[SerializeField] private AudioSource notificationSound;
+		[SerializeField] private GameObject notificationIcon;
+		[SerializeField] private GameObject notificationMessage;
+
+		public Action<bool> onChoiceSelected = delegate {};
+
+		public void IncomingTransmition(string info, string goodOption, string badOption) {
+			if(notificationSound != null) {
+				notificationSound.Play();
+			}
+
+			notificationIcon.SetActive(true);
+			notificationMessage.SetActive(true);
+			//notificationMessage.SetMessage(info)
+			//notificationMessage.SetGoodOption(goodOption)
+			//notificationMessage.SetBadOption(badOption)
+		}
+
+		public void SelectChoice(bool isGoodChoice) {
+			onChoiceSelected(isGoodChoice);
+		}
+	}
+}
