dce7f73eded36762e76eb72b323e672260363d9f
Thomas Haygarth <haygarth.thomas@gmail.com>
2018-01-28 11:42:55 +0000

    Created a prefab out of the email popup notification and added a class so the data can be set

---
+++ Basement Hacker/Assets/Scripts/UI/EmailPopup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace HacOS.Scripts.UI {
+	public class EmailPopup : MonoBehaviour {
+		[SerializeField] private TMP_Text subject;
+		[SerializeField] private TMP_Text message;
+
+		private const string subjectTitle = "Subject: {0}";
+
+		public void SetEmailText(string subject, string message) {
+			var sb = new StringBuilder();
+			sb.AppendFormat(subjectTitle, subject);
+			this.subject.text = sb.ToString();
+
+			this.message.text = message;
+		}
+	}
+}
